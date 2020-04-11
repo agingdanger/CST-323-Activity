0e857717a5056a47ef357b895c405d0420a11599
Charles Henderson <39602704+agingdanger@users.noreply.github.com>
2020-04-11 01:10:11 -0700

    Update MyLogger1.php

--- shod/app/Utility/MyLogger1.php
+++ shod/app/Utility/MyLogger1.php
@@ -16,7 +16,7 @@ class MyLogger1 implements ILogger
         if (self::$logger == null)
         {
             self::$logger = new Logger('MyApp');
-            $stream = new StreamHandler('storage/logs/myapp.log', Logger::DEBUG);
+            $stream = new StreamHandler(__DIR__.'/../../storage/logs/myapp.log', Logger::DEBUG);
             $stream->setFormatter(new LineFormatter("%datetime% : %level_name% : %message% %context%\n", "g:iA n/j/Y"));
             self::$logger->pushHandler($stream);
         }
